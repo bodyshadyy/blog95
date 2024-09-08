e22e7abd3e6ae134f46d2b8b59c6d60350484242
boody <bodyshady20@gmail.com>
2024-09-08 21:41:09 +0300

    Refactor app.js and app.css imports

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,18 +3,26 @@
 
         <h1 class="text-3xl font-bold mt-4">Latest Posts</h1>
         <div class="grid grid-cols-3 gap-4">
-            @foreach ($posts as $post)
-                <a href="/posts/{{ $post->id }}">
-                <div class="bg-white p-4 shadow rounded ">
-                    <h2 class="text-xl font-bold ">{{ $post->title }}</h2>
-                    <p>{{ $post->excerpt }}</p>
-                    <p class="text-sm mt>2">By {{ $post->user->name }}</p>
-                    {{ $post->Tags->pluck('name')->join(', ') }}
-                    <p class="text-sm mt-2">comments: {{ count($post->comments) }}</p>
+            <div class="bg-white py-24 sm:py-32">
+                <div class="mx-auto max-w-7xl px-6 lg:px-8">
+                    <div class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16  sm:mt-16  lg:mx-0 lg:max-w-none lg:grid-cols-3">
+                        @foreach ($posts as $post)
+                             <x-post></x-post>
+                            <a href="/posts/{{ $post->id }}">
+                            <div class="bg-white p-4 shadow rounded ">
+                                <h2 class="text-xl font-bold ">{{ $post->title }}</h2>
+                                <p>{{ $post->excerpt }}</p>
+                                <p class="text-sm mt>2">By {{ $post->user->name }}</p>
+                                {{ $post->Tags->pluck('name')->join(', ') }}
+                                <p class="text-sm mt-2">comments: {{ count($post->comments) }}</p>
+                            </div>
+                            </a>
+                        @endforeach
+                        </div>
+                    </div>
                 </div>
-                </a>
-            @endforeach
         </div>
+        <x-post></x-post>
     </div>
 
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,11 @@ Route::get('/posts/{post}', function (Post $post) {
         'comments' => $comments,
     ]);
 });
-
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 Route::get('/about', function () {
     return view('about');
 });
